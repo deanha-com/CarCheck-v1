f00c37a002fc6e69a1f31b897fd1a29a2c281c36
Dean Ha <dwha88@gmail.com>
2016-01-02 00:53:07 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CarCheck-v1
 A simple app using iframe
+
+* html
+* bootstrap
+* jQuery
---
+++ ccb.js
@@ -0,0 +1,13 @@
+// TOGGLES PANEL MOT-Status																	
+	$(document).ready(function(){
+    $("#msBtn").click(function(){
+        $("#motStatus").toggle();
+	  });
+	});
+
+// TOGGLES PANEL MOT-History
+	$(document).ready(function(){
+    $("#mhBtn").click(function(){
+        $("#motHistory").toggle();
+	  });
+	});
